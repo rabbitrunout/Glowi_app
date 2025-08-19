fa47301fde92355014cc589c4ce2fbb3d8b3902d
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-08-19 16:15:50 -0400

    render

--- database.php
+++ database.php
@@ -1,16 +1,17 @@
 <?php
-$host = 'localhost';
-$db   = 'glowi';
-$user = 'root';
-$pass = ''; // ← замени на свой пароль
+$host = getenv("DB_HOST");
+$db   = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
 
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8";
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("DB Connection failed: " . $e->getMessage());
 }
 ?>
